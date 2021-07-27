0f74576419300a5336ec1a54f76afd7242bf84fc
Francesco <francescococlite@gmail.com>
2021-07-27 15:46:18 +0200

    fine esercizio con dettagli grafici

--- google.php
+++ google.php
@@ -40,26 +40,29 @@ $lunghezza= count($faq);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>FAQ Google</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;1,300&display=swap" rel="stylesheet">
 </head>
 <body>
      <header>
          <div class="container-fluid">
              <div class="row mt-3">
-                 <div class="col-12 d-flex">
-                     <div class='logo'>
+                 <div class="col-11 d-flex">
+                     <div class='logo mt-1'>
                          <img src="img/logo-google.png" alt="google-logo">
                      </div>
                      <h1 class="fs-4 text-muted mx-2">Privacy e termini</h1>
-                     <div class="icone">
+                 </div>
+                 <div class="col-1 icone">
                       <i class="fas fa-th"></i>
                       <i class="fas fa-user-circle"></i>
-                     </div>
-                 </div>
+                </div>
              </div>
      
-             <div class="row mt-3">
-                 <div class="col-12 ">
+             <div class="row mt-1">
+                 <div class="col-12">
                      <nav>
                          <ul class="d-inline">
                              <li><a href="#">Introduzione</a></li>
