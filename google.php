1c31f26d2f028c8f7ca8306e0050daa18b8e8db5
Francesco <francescococlite@gmail.com>
2021-07-27 14:17:13 +0200

    formattazione pagina

---
+++ google.php
@@ -0,0 +1,60 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>FAQ Google</title>
+</head>
+<body>
+     <header>
+         <div class="container-fluid">
+             <div class="row mt-3">
+                 <div class="col-4 d-flex">
+                     <div class='logo'>
+                         <img src="img/logo-google.png" alt="google-logo">
+                     </div>
+                     <h1 class="fs-4 text-muted">Privacy e termini</h1>
+                 </div>
+             </div>
+     
+             <div class="row mt-3">
+                 <div class="col-12 ">
+                     <nav>
+                         <ul class="d-inline">
+                             <li><a href="#">Introduzione</a></li>
+                             <li><a href="#">Norme sulla privacy</a></li>
+                             <li><a href="#">Termini di servizio</a></li>
+                             <li><a href="#">Tecnologie</a></li>
+                             <li><a href="#">Domande Frequenti</a></li>
+                         </ul>
+                     </nav>
+                 </div>
+             </div>
+             <hr>
+         </div>
+     </header>
+    
+    <main>
+        <div class="container-lg p-5">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="mt-5 domanda">Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?</h2>
+                    <p class="risposta">
+                    La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi. <br>
+
+                    Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni. <br>
+
+                    Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.  <br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    
+</body>
+</html>
